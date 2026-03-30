f907dba46dfe2ea184668f2ece6986b8aaf4b7b1
hyein89 <bihontai@gmail.com>
2026-03-30 15:58:28 +0700

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer>
-      <Link href="#">Terms of Service</Link>
+      <Link href="/terms">Terms of Service</Link>
       <Link href="#">Privacy Policy</Link>
       <Link href="#">DMCA</Link>
     </footer>
