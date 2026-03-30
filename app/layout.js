cd77387ee6ec8a41334fdb4ccaf3fadadba81fb2
hyein89 <bihontai@gmail.com>
2026-03-30 13:47:38 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="id">
-      <body style={{ margin: 0, padding: 0, fontFamily: 'system-ui, sans-serif', backgroundColor: '#f4f4f5' }}>
-        {children}
+    <html lang="en">
+      <body>
+       {children}
       </body>
     </html>
   )
